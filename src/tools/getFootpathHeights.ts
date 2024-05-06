4a27492665ec1613f67e8b93591191e980f86ad0
Smitty Penman <smitty.penman@gmail.com>
2024-05-06 08:55:25 -0400

    fix: always allow surface selection

--- src/tools/getFootpathHeights.ts
+++ src/tools/getFootpathHeights.ts
@@ -2,11 +2,10 @@ export const getFootpathHeights = (coords: CoordsXY | null) => {
   if (!coords) return [];
 
   const elements = map.getTile(coords.x / 32, coords.y / 32).elements;
-  let heights = elements.filter((element) => element.type === "footpath").map((path) => path.baseZ);
+  let pathHeights = elements.filter((element) => element.type === "footpath").map((path) => path.baseZ);
+  let surfaceHeights = elements.filter((element) => element.type === "surface").map((surface) => surface.baseZ);
 
-  if (heights.length === 0) {
-    heights = elements.filter((element) => element.type === "surface").map((surface) => surface.baseZ);
-  }
-
-  return [...heights].sort((a, b) => a - b);
+  const heights = [...pathHeights, ...surfaceHeights];
+  const uniqueHeights = heights.filter((height, index) => heights.indexOf(height) === index);
+  return uniqueHeights.sort((a, b) => a - b);
 };
